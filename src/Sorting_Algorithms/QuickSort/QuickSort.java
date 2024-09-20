87863b93c749f91a1509cf1fdd6db5d991fce0f9
sefasezr <153550790+sefasezr@users.noreply.github.com>
2024-09-20 22:16:45 +0300

    20.09.2024

--- src/Sorting_Algorithms/QuickSort/QuickSort.java
+++ src/Sorting_Algorithms/QuickSort/QuickSort.java
@@ -4,7 +4,7 @@ public class QuickSort {
 
     public static int partition(int dizi[], int alt, int ust ){
         int pivot = dizi[ust];
-        int i = (alt - 1);
+        int i = (alt - 1);gi
     }
 
     // ana metod
